882d38df6fff3af597551f8000b84a3e07ba6023
Spaceoceanoutlook <spaceocean@outlook.com>
2023-07-12 09:17:23 +0500

    add db

--- WordGame/word_game/service.py
+++ WordGame/word_game/service.py
@@ -1,2 +1,2 @@
 with open('word_game/DB.txt', 'r', encoding='utf-8') as f:
-    comp_words = [line[:-1] for line in f.readlines()]
+    all_comp_words = [line[:-1] for line in f.readlines()]
--- WordGame/word_game/views.py
+++ WordGame/word_game/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render, redirect
-from .service import comp_words
+from .service import all_comp_words
 
 
 my_words = []
-answer = []
+comp_words = []
 LONG_WORD = ''
 
 
-def check_user_words(arr, comp):
-    if len(arr) > len(comp):
-        return f'Количество твоих слов: {len(arr)}, компьютера: {len(comp)}. Вы победили!'
-    elif len(arr) < len(comp):
-        return f'Количество твоих слов: {len(arr)}, компьютера: {len(comp)}. Вы проиграли!'
+def check_user_words(my_arr, comp_arr):
+    text = f'Количество твоих слов: {len(my_arr)}, компьютера: {len(comp_arr)}. '
+    if len(my_arr) > len(comp_arr):
+        return f'{text} Вы победили!'
+    elif len(my_arr) < len(comp_arr):
+        return f'{text} Вы проиграли!'
     else:
-        return f'Количество твоих слов: {len(arr)}, компьютера: {len(comp)}. Ничья!'
+        return f'{text} Ничья!'
 
 
 def main(request):
@@ -24,18 +25,18 @@ def main(request):
     return render(request, 'word_game/main.html')
 
 
-def search_words():
-    for word in comp_words:
+def search_words(arr):
+    for word in arr:
         for symbol in word:
             if symbol in LONG_WORD and LONG_WORD.count(symbol) >= word.count(symbol):
                 pass
             else:
                 break
         else:
-            answer.append(word)
-    if len(answer) == 0:
+            comp_words.append(word)
+    if len(comp_words) == 0:
         return 'Я не составил ни одного слова'
-    return f"{', '.join(answer)}"
+    return f"{', '.join(comp_words)}"
 
 
 def game(request):
@@ -44,15 +45,13 @@ def game(request):
         my_words.append(word)
         return redirect('game')
     if request.method == 'POST' and 'check' in request.POST:
-        ans = search_words()
-        ch = check_user_words(my_words, answer)
-        context = {'ans': ans, 'ch': ch, 'my_words': ', '.join(my_words)}
+        result_search_words = search_words(all_comp_words)
+        result_check = check_user_words(my_words, comp_words)
+        string_my_words = ', '.join(my_words)
+        context = {'result_search_words': result_search_words, 'result_check': result_check,
+                   'string_my_words': string_my_words}
         return render(request, 'word_game/check.html', context=context)
     else:
         context = {'my_word': my_words, 'LONG_WORD': LONG_WORD}
         return render(request, 'word_game/game.html', context=context)
-
-
-
-
 
